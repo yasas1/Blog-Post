f389ab9ecfe3d7cc024b01a5018f7d983a7e0eac
Yasas Ranawaka <ranawaka.y@gmail.com>
2022-02-18 01:52:38 +0530

    post controller and service

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
@@ -1,7 +1,30 @@
 package com.springboot.blog.controller;
 
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/posts")
 public class PostController {
+
+    private final PostService postService;
+
+    @Autowired
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    // create post api
+    @PostMapping
+    public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
+        return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
 }
