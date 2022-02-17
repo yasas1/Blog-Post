35f2163bc9dae73180ddcfc804b276f5708c7d14
Yasas Ranawaka <ranawaka.y@gmail.com>
2022-02-18 01:38:57 +0530

    post controller and service

---
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -0,0 +1,38 @@
+package com.springboot.blog.service.impl;
+
+import com.springboot.blog.entity.Post;
+import com.springboot.blog.payload.PostDto;
+import com.springboot.blog.repository.PostRepository;
+import com.springboot.blog.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+    private PostRepository postRepository;
+
+    @Autowired
+    public PostServiceImpl(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+
+        Post savedPost = postRepository.save(post);
+
+        PostDto savedPostDto = new PostDto();
+        savedPostDto.setId(savedPost.getId());
+        savedPostDto.setTitle(savedPost.getTitle());
+        savedPostDto.setDescription(savedPost.getDescription());
+        savedPostDto.setContent(savedPost.getContent());
+
+        return savedPostDto;
+    }
+}
