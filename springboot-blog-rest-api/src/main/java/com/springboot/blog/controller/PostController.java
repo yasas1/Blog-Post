1505f48f4011dc3f03132ffb12f965b232b867ad
Yasas Ranawaka <ranawaka.y@gmail.com>
2022-03-09 02:30:14 +0530

    get all with paging

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
@@ -3,6 +3,7 @@ package com.springboot.blog.controller;
 import com.springboot.blog.payload.PostDto;
 import com.springboot.blog.payload.ResultResponse;
 import com.springboot.blog.service.PostService;
+import com.springboot.blog.utils.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -29,10 +30,10 @@ public class PostController {
 
     //get all posts api
     @GetMapping
-    public ResultResponse<PostDto> getAllPosts(@RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-                                               @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-                                               @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-                                               @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir){
+    public ResultResponse<PostDto> getAllPosts(@RequestParam(value = "pageNo", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+                                               @RequestParam(value = "pageSize", defaultValue = AppConstants.DEFAULT_PAGE_SIZE, required = false) int pageSize,
+                                               @RequestParam(value = "sortBy", defaultValue = AppConstants.DEFAULT_SORT_BY, required = false) String sortBy,
+                                               @RequestParam(value = "sortDir", defaultValue = AppConstants.DEFAULT_SORT_DIR, required = false) String sortDir){
         return this.postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
 
