cc8733b78e808e80743fe3a1188c82c432a30f58
Yasas Ranawaka <ranawaka.y@gmail.com>
2022-04-10 19:24:02 +0530

    global exception handler for resource not found exception

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/exception/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.springboot.blog.payload.ErrorDetails;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
@@ -11,9 +12,16 @@ import java.util.Date;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<ErrorDetails> handleResourceNotFoundException(ResourceNotFoundException exception, WebRequest webRequest){
         ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(BlogAPIException.class)
+    public ResponseEntity<ErrorDetails> handleResourceNotFoundException(BlogAPIException exception, WebRequest webRequest){
+        ErrorDetails errorDetails = new ErrorDetails(new Date(), exception.getMessage(), webRequest.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 
 }
