1299b1daddf2bbd0eba0e813dd6c1778c354f0eb
Yasas Ranawaka <ranawaka.y@gmail.com>
2022-06-17 16:59:55 +0530

    Spring security configuration

--- springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
+++ springboot-blog-rest-api/src/main/java/com/springboot/blog/controller/PostController.java
@@ -7,6 +7,7 @@ import com.springboot.blog.utils.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +32,7 @@ public class PostController {
     }
 
     // create post api
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
     public ResponseEntity<PostDto> createPost(@Valid @RequestBody PostDto postDto){
         return new ResponseEntity<>(this.postService.createPost(postDto), HttpStatus.CREATED);
@@ -52,12 +54,14 @@ public class PostController {
     }
 
     // update post by id
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostDto> updatePost(@Valid @RequestBody PostDto postDto, @PathVariable(name = "id") long id){
         return new ResponseEntity<>(this.postService.updatePost(postDto, id), HttpStatus.OK);
     }
 
     // delete post by id
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deletePostById(@PathVariable(name = "id") long id){
         this.postService.deletePostById(id);
